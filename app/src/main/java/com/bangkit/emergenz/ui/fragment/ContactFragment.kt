44b83889f904dde41a9609815b38f375b3c30293
AhmadGhoni <ahmad297y@gmail.com>
2023-05-24 13:53:03 +0700

    Add fragment article & fragment contact

---
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/ContactFragment.kt
@@ -0,0 +1,30 @@
+package com.bangkit.emergenz.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bangkit.emergenz.databinding.FragmentContactBinding
+
+class ContactFragment : Fragment() {
+    private var _binding: FragmentContactBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
