a07d77c8fe6b1dcc9dd5329147df3802b5f8a7bc
AhmadGhoni <ahmad297y@gmail.com>
2023-05-24 13:53:03 +0700

    Add fragment article & fragment contact

---
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.bangkit.emergenz.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bangkit.emergenz.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        testButton()
+    }
+
+    private fun testButton(){
+        binding.button.setOnClickListener {
+            val bottomSheetFragment = CallPageFragment()
+            bottomSheetFragment.show(requireActivity().supportFragmentManager, "CallPageFragment")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
