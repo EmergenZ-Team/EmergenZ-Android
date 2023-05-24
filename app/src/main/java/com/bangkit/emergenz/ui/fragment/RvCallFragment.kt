85febf35dda0b1366b53cce9c55abeca0209bff8
AhmadGhoni <ahmad297y@gmail.com>
2023-05-24 16:14:54 +0700

    Add pop up layout

--- app/src/main/java/com/bangkit/emergenz/ui/fragment/RvCallFragment.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/RvCallFragment.kt
@@ -1,10 +1,17 @@
 package com.bangkit.emergenz.ui.fragment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.bangkit.emergenz.R
 import com.bangkit.emergenz.databinding.FragmentRvCallBinding
 
 class RvCallFragment : Fragment() {
@@ -21,6 +28,28 @@ class RvCallFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.button2.setOnClickListener {
+            setCustomDialogBox()
+        }
+    }
+
+    private fun setCustomDialogBox(){
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.card_alert)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnYes: Button = dialog.findViewById(R.id.btn_yes)
+        val btnNo: Button = dialog.findViewById(R.id.btn_no)
+
+        btnYes.setOnClickListener {
+            Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+        }
+        btnNo.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 
     override fun onDestroy() {
