e3037832044e1c4779a8fe660c74984d2ce2a6a3
Gregorian <gregorianmargono@gmail.com>
2023-05-25 15:28:33 +0700

    Implement login testing logic. API is not yet implemented, so we just use intent instead of doing retrofit things.

---
+++ app/src/main/java/com/bangkit/emergenz/ui/activity/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.bangkit.emergenz.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.emergenz.R
+import com.bangkit.emergenz.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
