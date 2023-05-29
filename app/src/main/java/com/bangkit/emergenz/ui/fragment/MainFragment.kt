d806ba8956954875fce4f3d2d0b5dc0b465e0cb4
Gregorian <gregorianmargono@gmail.com>
2023-05-29 20:34:05 +0700

    Overall improvement to layout and stuff but still crash due to fragment using intent.

--- app/src/main/java/com/bangkit/emergenz/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/MainFragment.kt
@@ -24,6 +24,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         testButton()
+        topNavMenu()
     }
 
     private fun testButton(){
