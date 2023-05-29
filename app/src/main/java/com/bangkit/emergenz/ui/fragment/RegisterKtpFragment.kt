4a8ef94f40297e4bc1b9a789a8a7d45a35d9e437
Gregorian <gregorianmargono@gmail.com>
2023-05-29 12:05:15 +0700

    Added Camera Intent from StoryApp and attempting to convert it into fragment in form of fragment_camera.xml

---
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/RegisterKtpFragment.kt
@@ -0,0 +1,35 @@
+package com.bangkit.emergenz.ui.fragment
+
+import android.Manifest
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.emergenz.R
+import com.bangkit.emergenz.databinding.FragmentRegisterKtpBinding
+
+class RegisterKtpFragment : Fragment() {
+    private var _binding: FragmentRegisterKtpBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentRegisterKtpBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+}
