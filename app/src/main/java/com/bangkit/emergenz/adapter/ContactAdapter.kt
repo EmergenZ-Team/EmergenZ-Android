243ee1b84080e1682fe4915c4f7ef13e094f9c52
Gregorian <gregorianmargono@gmail.com>
2023-06-03 16:13:41 +0700

    Adding popup dialog, Not yet adding the "Internal Intent" to actually call the contact

---
+++ app/src/main/java/com/bangkit/emergenz/adapter/ContactAdapter.kt
@@ -0,0 +1,39 @@
+package com.bangkit.emergenz.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.emergenz.data.local.model.Contact
+import com.bangkit.emergenz.databinding.CardTelpBinding
+
+class ContactAdapter(private val listContact: List<Contact>) :
+    RecyclerView.Adapter<ContactAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+
+    class ListViewHolder(var binding: CardTelpBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = CardTelpBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun getItemCount() = listContact.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val contact = listContact[position]
+        holder.binding.apply {
+            tvNamePlace.text = contact.name
+            tvNumberPhone.text = contact.number
+            ivCall.setOnClickListener{onItemClickCallback.onItemClicked(listContact[holder.bindingAdapterPosition])}
+        }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Contact)
+    }
+}
--- app/src/main/java/com/bangkit/emergenz/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/activity/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.bangkit.emergenz.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import com.bangkit.emergenz.R
 import com.bangkit.emergenz.databinding.ActivityMainBinding
+import com.bangkit.emergenz.ui.fragment.ContactFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,15 +30,49 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.action_home -> findNavController(R.id.nav_host_fragment_content_main).popBackStack(
                     R.id.mainFragment, false)
-                R.id.action_contact -> findNavController(R.id.nav_host_fragment_content_main).navigate(
-                    R.id.contactFragment
-                )
+                R.id.action_contact ->
+                    if (!allPermissionsGranted()) {
+                        ActivityCompat.requestPermissions(
+                            this,
+                            REQUIRED_PERMISSIONS,
+                            REQUEST_CODE_PERMISSIONS
+                        )
+                    } else {
+                        findNavController(R.id.nav_host_fragment_content_main).navigate(
+                            R.id.contactFragment)
+                    }
                 R.id.action_article -> findNavController(R.id.nav_host_fragment_content_main).navigate(
                     R.id.articleFragment
                 )
             }
             true
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.perm_lack),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this.baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.READ_CONTACTS)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 }
