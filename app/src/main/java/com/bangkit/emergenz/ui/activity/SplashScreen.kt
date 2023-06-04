41d50d47250677f9bc30ed85fba9271c148b55fc
Ahmad Ghoni Arrozaq <93740880+AhmadGhoni@users.noreply.github.com>
2023-06-04 12:47:49 +0700

    Add logo splashscreen (#18)
    * Fix minor bug
    
    * Add dummy article
    
    * Add logo splashscreen

--- app/src/main/java/com/bangkit/emergenz/ui/activity/SplashScreen.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/activity/SplashScreen.kt
@@ -1,11 +1,14 @@
 package com.bangkit.emergenz.ui.activity
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
 import com.bangkit.emergenz.R
 import com.bangkit.emergenz.databinding.ActivitySplashScreenBinding
 
@@ -20,17 +23,36 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        moveToMain()
         startup()
     }
 
+    @Suppress("DEPRECATION")
     private fun startup() {
+        val imgSplashScreen: ImageView = findViewById(R.id.img_splash_screen)
+
+        val initialX = -imgSplashScreen.width.toFloat()
+        val finalX = (windowManager.defaultDisplay.width - imgSplashScreen.width) / 4f
+
+        // Create the ObjectAnimator
+        val animator = ObjectAnimator.ofFloat(imgSplashScreen, "translationX", initialX, finalX)
+        animator.duration = LOGO_MOVE
+        animator.interpolator = AccelerateDecelerateInterpolator()
+
+        // Start the animation
+        animator.start()
+    }
+
+    private fun moveToMain(){
         Handler(Looper.getMainLooper()).postDelayed({
-                val i = Intent(this@SplashScreen, MainActivity::class.java)
-                startActivity(i)
+            val i = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(i)
             finish()
         }, STARTUP_TIME)
     }
+
     companion object{
-        const val STARTUP_TIME : Long = 2000
+        const val LOGO_MOVE : Long = 600
+        const val STARTUP_TIME : Long = 1400
     }
 }
