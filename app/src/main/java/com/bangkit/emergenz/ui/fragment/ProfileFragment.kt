ae41780de12e6605e9258d6c461fe9889d83794d
Gregorian Margono <gregorianmargono@gmail.com>
2023-06-06 21:01:13 +0700

    Added Login and Logout Implementation (#20)
    * Adding ViewModel for Register and Login, APIServices(Cloud) and APIConfig(Cloud)
    
    * Small Fix : Adding the APIServiceCloud for APIConfigCloud
    
    * Application now save token and user information. When user's log in, user will no longer greeted by Auth Activity for next time they visit the app. This changes will no longer infuriate tester as well
    
    Not yet implement the logout
    
    * Simple logout implemented
    
    * Adding the unshelved changes

--- app/src/main/java/com/bangkit/emergenz/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/ProfileFragment.kt
@@ -1,17 +1,29 @@
 package com.bangkit.emergenz.ui.fragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.bangkit.emergenz.R
+import com.bangkit.emergenz.data.local.datastore.UserPreferences
 import com.bangkit.emergenz.databinding.FragmentProfileBinding
+import com.bangkit.emergenz.ui.activity.AuthActivity
+import com.bangkit.emergenz.ui.viewmodel.ProfileViewModel
+import com.bangkit.emergenz.ui.viewmodel.ViewModelFactory
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
 class ProfileFragment : Fragment() {
     private var _binding : FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var profileViewModel : ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +39,23 @@ class ProfileFragment : Fragment() {
         requireActivity().actionBar?.title = "Profile"
         requireActivity().actionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val pref = UserPreferences.getInstance(requireContext().dataStore)
+        profileViewModel = ViewModelProvider(requireActivity(), ViewModelFactory(pref))[ProfileViewModel::class.java]
+
         binding.btnIdRegister.setOnClickListener{
             view.findNavController().navigate(R.id.action_profileFragment_to_registerKtpFragment)
         }
+
+        binding.btnLogout.setOnClickListener{
+            logout()
+        }
+    }
+
+    private fun logout() {
+        val intent = Intent(requireActivity(), AuthActivity::class.java)
+        profileViewModel.saveToken("")
+        profileViewModel.setSession(false)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
