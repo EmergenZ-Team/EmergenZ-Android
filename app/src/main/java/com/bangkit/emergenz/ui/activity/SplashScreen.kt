37e15ec3d360112578e55b2e9c9d2382badccc63
AhmadGhoni <ahmad297y@gmail.com>
2023-06-07 23:46:59 +0700

    Add dark mode setting

--- app/src/main/java/com/bangkit/emergenz/ui/activity/SplashScreen.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/activity/SplashScreen.kt
@@ -3,20 +3,28 @@ package com.bangkit.emergenz.ui.activity
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
-import com.bangkit.emergenz.data.local.datastore.UserPreferences
-import android.widget.ImageView
+import androidx.lifecycle.lifecycleScope
 import com.bangkit.emergenz.R
+import com.bangkit.emergenz.data.local.datastore.UserPreferences
 import com.bangkit.emergenz.databinding.ActivitySplashScreenBinding
 import com.bangkit.emergenz.ui.viewmodel.TokenViewModel
 import com.bangkit.emergenz.ui.viewmodel.ViewModelFactory
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
 
@@ -48,16 +56,33 @@ class SplashScreen : AppCompatActivity() {
             if(onSession) {
                 val moveToMainActivity = Intent(this@SplashScreen, MainActivity::class.java)
                 startActivity(moveToMainActivity)
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                finish()
             }
             else{
                 val moveToAuthActivity = Intent(this@SplashScreen, AuthActivity::class.java)
                 startActivity(moveToAuthActivity)
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                finish()
+            }
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        setTheme()
+    }
+
+    private fun setTheme(){
+        lifecycleScope.launch {
+            when (getCurrentDarkModeValue()) {
+                "terang" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                "gelap" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
             }
         }
+    }
+
+    private suspend fun getCurrentDarkModeValue(): String {
+        val darkModeKey = stringPreferencesKey("dark_mode")
+        val darkModeValueFlow: Flow<String> = dataStore.data.map { preferences ->
+            preferences[darkModeKey] ?: "otomatis"
+        }
+        return darkModeValueFlow.first()
     }
 
     companion object{
