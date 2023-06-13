8b2ab6dd7608814ba80cd99592a6c8c2bf4b2992
AhmadGhoni <ahmad297y@gmail.com>
2023-06-13 21:52:51 +0700

    Add article

--- app/src/main/java/com/bangkit/emergenz/data/repository/ArticleRepository.kt
+++ app/src/main/java/com/bangkit/emergenz/data/repository/ArticleRepository.kt
@@ -1,12 +1,16 @@
 package com.bangkit.emergenz.data.repository
 
-import com.bangkit.emergenz.data.api.ArticleApiService
-import com.bangkit.emergenz.data.dao.ArticleDao
-import com.bangkit.emergenz.data.local.model.ArticleResponse
+import com.bangkit.emergenz.data.api.ApiServiceCloud
+import com.bangkit.emergenz.data.response.article.AddRecordResponse
+import com.bangkit.emergenz.data.response.article.RecomNewsResponse
 import retrofit2.Response
 
-class ArticleRepository(private val articleApiService: ArticleApiService, private val articleDao: ArticleDao) {
-    suspend fun fetchDataAndCache(): Response<ArticleResponse> {
-        return articleApiService.findArticle()
+class ArticleRepository(private val email: String, private val articleApiService: ApiServiceCloud) {
+    suspend fun fetchDataAndCache(): Response<RecomNewsResponse> {
+        return articleApiService.findArticle(email)
     }
+    suspend fun fetchDetail(news_id: String): Response<AddRecordResponse> {
+        return articleApiService.getDetailArticle(news_id ,email)
+    }
+
 }
