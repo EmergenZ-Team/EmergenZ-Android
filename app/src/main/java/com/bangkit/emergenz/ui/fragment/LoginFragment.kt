e2183b7e630ce2571725d930a52936c73e592e5f
Ahmad Ghoni Arrozaq <93740880+AhmadGhoni@users.noreply.github.com>
2023-06-14 07:18:35 +0700

    Fix minor bug (#37)
    * Add article
    
    * Fix paragraf article
    
    * Fix darkmode
    
    * Fix loading
    
    * Fix minor bug

--- app/src/main/java/com/bangkit/emergenz/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/LoginFragment.kt
@@ -1,17 +1,20 @@
 package com.bangkit.emergenz.ui.fragment
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.bangkit.emergenz.R
@@ -38,7 +41,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        playAnimation()
         val pref = UserPreferences.getInstance(requireContext().dataStore)
         loginViewModel = ViewModelProvider(requireActivity(), ViewModelFactory(pref))[LoginViewModel::class.java]
 
@@ -92,10 +95,12 @@ class LoginFragment : Fragment() {
         }
         if (isLoading) {
             binding.progressBarLogin.visibility = View.VISIBLE
+            binding.imageView.visibility = View.INVISIBLE
             binding.tvRegisterNow.isEnabled = false
             binding.btnLogin.isEnabled = false
         } else {
             binding.progressBarLogin.visibility = View.GONE
+            binding.imageView.visibility = View.VISIBLE
             binding.tvRegisterNow.isEnabled = true
             binding.btnLogin.isEnabled = true
         }
@@ -103,6 +108,47 @@ class LoginFragment : Fragment() {
 
     private fun showToast(fungus: String?) {
         Toast.makeText(requireActivity(), fungus, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun playAnimation() {
+        val logoImageView = binding.imageView
+        // Get the center X position of the screen
+        val centerX = resources.displayMetrics.widthPixels / 2
+
+        // Calculate the translation X values
+        val startTranslationX = -centerX + logoImageView.width.toFloat() / 2
+        val endTranslationX = -logoImageView.width.toFloat()
+
+        // Animation duration in milliseconds
+        val animationDuration = 800L
+
+        // Set the initial position
+        logoImageView.translationX = startTranslationX
+
+        // Start the animation
+        logoImageView.animate()
+            .translationX(endTranslationX)
+            .setDuration(animationDuration)
+            .setInterpolator(AccelerateDecelerateInterpolator())
+            .start()
+
+        val descTextVIew = ObjectAnimator.ofFloat(binding.tvEmail, View.ALPHA, 1f).setDuration(500)
+        val tvPassword = ObjectAnimator.ofFloat(binding.tvPassword, View.ALPHA, 1f).setDuration(500)
+        val tvRegister = ObjectAnimator.ofFloat(binding.tvRegisterNow, View.ALPHA, 1f).setDuration(500)
+
+        val etEmail = ObjectAnimator.ofFloat(binding.edLoginEmail, View.ALPHA, 1f).setDuration(500)
+        val etPwd = ObjectAnimator.ofFloat(binding.edLoginPassword, View.ALPHA, 1f).setDuration(500)
+
+        val btnLogin = ObjectAnimator.ofFloat(binding.btnLogin, View.ALPHA, 1f).setDuration(500)
+
+        val togetherBtn = AnimatorSet().apply {
+            playTogether(btnLogin,tvPassword,tvRegister,descTextVIew)
+        }
+        AnimatorSet().apply {
+            playSequentially(togetherBtn,etEmail,etPwd)
+            start()
+        }
+
     }
 
     override fun onDestroyView() {
