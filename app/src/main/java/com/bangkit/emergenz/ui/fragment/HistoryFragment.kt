5ac275c4387c210adf796aa6febcd79917713704
Ahmad Ghoni Arrozaq <93740880+AhmadGhoni@users.noreply.github.com>
2023-07-12 00:43:46 +0700

    Fix history (#46)
    * Add history
    
    * Fix history

--- app/src/main/java/com/bangkit/emergenz/ui/fragment/HistoryFragment.kt
+++ app/src/main/java/com/bangkit/emergenz/ui/fragment/HistoryFragment.kt
@@ -23,6 +23,7 @@ import com.bangkit.emergenz.databinding.FragmentHistoryBinding
 import com.bangkit.emergenz.ui.viewmodel.HistoryViewModel
 import com.bangkit.emergenz.ui.viewmodel.HistoryViewModelFactory
 import com.bangkit.emergenz.util.LastItemSpacingDecoration
+import com.bangkit.emergenz.util.animateVisibility
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -46,25 +47,29 @@ class HistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setToolbar()
+        showLoading(true)
 
         val apiService = ApiConfigCloud.getApiService()
         val historyRepository = HistoryRepository(apiService)
         val pref = UserPreferences.getInstance(requireContext().dataStore)
-        var data = emptyList<DataItem>()
 
         historyViewModel = ViewModelProvider(requireActivity(), HistoryViewModelFactory(historyRepository, pref))[HistoryViewModel::class.java]
         historyViewModel.getName().observe(viewLifecycleOwner){name->
-            historyViewModel.getHistory(name)
+            coroutineScope.launch{
+                delay(500)
+                loadNotFound(false)
+                historyViewModel.chckErr.observe(viewLifecycleOwner){
+                    loadNotFound(it)
+                }
+                historyViewModel.getHistory(name)
+                showLoading(false)
+            }
         }
         historyViewModel.listHistory.observe(viewLifecycleOwner){newData->
-            data = newData
+            setDataToFragment(newData)
         }
         historyViewModel.txtErr.observe(viewLifecycleOwner){
             Log.d("DATA", "$it")
-        }
-        coroutineScope.launch{
-            delay(2000)
-            setDataToFragment(data)
         }
     }
 
@@ -83,6 +88,26 @@ class HistoryFragment : Fragment() {
             recyclerView.adapter = adapter
             val itemDecoration = LastItemSpacingDecoration(290)
             recyclerView.addItemDecoration(itemDecoration)
+        }
+    }
+
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.apply {
+            // Animate views alpha
+            if (isLoading) {
+                loadingBar6.animateVisibility(true)
+            } else {
+                loadingBar6.animateVisibility(false)
+            }
+        }
+    }
+
+    private fun loadNotFound(notFound: Boolean){
+        if (notFound){
+            binding.tvNotFound.visibility = View.VISIBLE
+        }else{
+            binding.tvNotFound.visibility = View.GONE
         }
     }
 
